ba2731b068ee3fca3d830450444754b955a4e83b
Quillbert <quillbert.quillbert@gmail.com>
2019-09-12 10:48:33 -0400

    Cards of non current player now are hidden

--- public/player.js
+++ public/player.js
@@ -5,10 +5,16 @@ class Player {
 		this.cards = [];
 	}
 	show() {
-		for (let i = 0; i < this.cards.length; i++) {
-			this.cards[i].x = this.x + (i%15) * 25;
-			this.cards[i].y = this.y + floor(i/15) * 35;
-			this.cards[i].show();
+		if(players[turn] === this) {
+			for (let i = 0; i < this.cards.length; i++) {
+				this.cards[i].x = this.x + (i%15) * 25;
+				this.cards[i].y = this.y + floor(i/15) * 35;
+				this.cards[i].show();
+			}
+		} else {
+			for(let i = 0; i < this.cards.length; i++) {
+				image(deckImage, (i%15) * 25, floor(i/15) * 35);
+			}
 		}
 	}
 	play() {
